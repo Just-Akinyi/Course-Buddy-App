519dbca6d2817c7dd5e4eb103dc03de88105cb81
Justin Akinyi <justakinyi1@gmail.com>
2025-08-28 19:04:49 +0300

    Add source code and configs

---
+++ coursebuddy/android/build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file for Flutter project
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Required for Firebase
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
